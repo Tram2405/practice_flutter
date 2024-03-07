cbeadca799087db5491cb455e66708912603c4ea
Tram2405 <tram24052002@gmail.com>
2024-03-07 16:01:21 +0700

    fix: fix comments: login,register, forgot pass controller...

--- task_manager_flutter/lib/controller/auth/register_controller.dart
+++ task_manager_flutter/lib/controller/auth/register_controller.dart
@@ -6,22 +6,20 @@ class RegisterController extends GetxController {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController retypePasswordController = TextEditingController();
-  
+  final TextEditingController retypePasswordController =
+      TextEditingController();
+
   RxBool hasContent = false.obs;
   RxBool isShowPassword = false.obs;
 
   void isEmpty() {
-    if (FormValidator.validatorRequired(nameController.text) == null &&
-        FormValidator.validatorEmail(emailController.text) == null &&
-        FormValidator.validatorPassword(passwordController.text) == null &&
-        FormValidator.validatorConfirmPassword(
-                retypePasswordController.text, passwordController.text) ==
-            null) {
-      hasContent.value = true;
-    } else {
-      hasContent.value = false;
-    }
+    hasContent.value =
+        FormValidator.validatorRequired(nameController.text) == null &&
+            FormValidator.validatorEmail(emailController.text) == null &&
+            FormValidator.validatorPassword(passwordController.text) == null &&
+            FormValidator.validatorConfirmPassword(
+                    retypePasswordController.text, passwordController.text) ==
+                null;
   }
 
   void changeStatePassword() {

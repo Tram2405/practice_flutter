b2b4db21634c5176350153b432dc4e7329d77c3e
Tram2405 <tram24052002@gmail.com>
2024-03-13 17:32:07 +0700

    fix: update edit subtask

--- task_manager_flutter/lib/ui/manager/detail_task_page.dart
+++ task_manager_flutter/lib/ui/manager/detail_task_page.dart
@@ -25,72 +25,74 @@ class DetailTaskPage extends GetView<DetailTaskController> {
     const sizedBox16 = SizedBox(height: 16.0);
     controller.getSubTask(task);
 
-    return TMScaffold(
-      backgroundColor: TMColor.onSecondary,
-      appBar: TMAppbar(
-        title: 'Detail Task',
-        leftIcon: Assets.icons.iconArrowLeft,
-        leftPressed: () => Get.back(),
-        rightIcon: Assets.icons.iconComment,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            controller.task.value.typeTask.toStyleTaskDisplay(context),
-            sizedBox12,
-            TMTitle(
-              title: controller.task.value.nameTask ?? '--:--',
-              textStyle: context.textTheme.displaySmall,
-            ),
-            sizedBox12,
-            TMDisplayDateTime(
-                title: 'Start Date',
-                dateTime: controller.task.value.subTasks?[0].dueDate.toDateTime ?? '--:--'),
-            sizedBox12,
-            TMTitle(
-              title: controller.task.value.description ?? '',
-              textStyle: context.textTheme.bodyMedium,
-            ),
-            sizedBox12,
-            TMTitle(
-              title: 'Progress',
-              textStyle: context.textTheme.labelLarge,
-            ),
-            sizedBox16,
-            TMPercentTask(
-              percent: task.getPercentCompleted(),
-            ),
-            sizedBox16,
-            TMTitle(
-              title: 'SubTasks',
-              textStyle: context.textTheme.labelLarge,
-            ),
-            sizedBox12,
-            TMButtonTask(
-              onPressed: () {
-                Get.toNamed(Routes.ADD_SUB_TASK);
-              },
-              text: 'Add SubTask',
-              leftIcon: Assets.icons.iconAdd,
-              leftIconColor: TMColor.background,
-            ),
-            sizedBox16,
-            ListView.separated(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: controller.task.value.subTasks?.length ?? 0,
-              itemBuilder: (context, index) {
-                final subTask = controller.task.value.subTasks?[index] ?? SubTaskModel();
-                return TMCardSubTask(onTap: () {
-                  Get.toNamed(Routes.DETAIL_SUB_TASK, arguments: [subTask]);
-                }, subTask: subTask,index: index,);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                height: 6.0,
+    return Obx(
+      () =>  TMScaffold(
+        backgroundColor: TMColor.onSecondary,
+        appBar: TMAppbar(
+          title: 'Detail Task',
+          leftIcon: Assets.icons.iconArrowLeft,
+          leftPressed: () => Get.back(),
+          rightIcon: Assets.icons.iconComment,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              controller.task.value.typeTask.toStyleTaskDisplay(context),
+              sizedBox12,
+              TMTitle(
+                title: controller.task.value.nameTask ?? '--:--',
+                textStyle: context.textTheme.displaySmall,
               ),
-            )
-          ],
+              sizedBox12,
+              TMDisplayDateTime(
+                  title: 'Start Date',
+                  dateTime: controller.task.value.subTasks?[0].dueDate.toDateTime ?? '--:--'),
+              sizedBox12,
+              TMTitle(
+                title: controller.task.value.description ?? '',
+                textStyle: context.textTheme.bodyMedium,
+              ),
+              sizedBox12,
+              TMTitle(
+                title: 'Progress',
+                textStyle: context.textTheme.labelLarge,
+              ),
+              sizedBox16,
+              TMPercentTask(
+                percent: task.getPercentCompleted(),
+              ),
+              sizedBox16,
+              TMTitle(
+                title: 'SubTasks',
+                textStyle: context.textTheme.labelLarge,
+              ),
+              sizedBox12,
+              TMButtonTask(
+                onPressed: () {
+                  Get.toNamed(Routes.ADD_SUB_TASK);
+                },
+                text: 'Add SubTask',
+                leftIcon: Assets.icons.iconAdd,
+                leftIconColor: TMColor.background,
+              ),
+              sizedBox16,
+              ListView.separated(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: controller.task.value.subTasks?.length ?? 0,
+                itemBuilder: (context, index) {
+                  final subTask = controller.task.value.subTasks?[index] ?? SubTaskModel();
+                  return TMCardSubTask(onTap: () {
+                    Get.toNamed(Routes.DETAIL_SUB_TASK, arguments: [subTask]);
+                  }, subTask: subTask,index: index,);
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 6.0,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
